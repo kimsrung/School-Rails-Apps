0e73436e242cabfc0d6878f5f9d9f6ee3bfabb73
kimsrung <kimsrung.lov@gmail.com>
2013-09-19 17:13:57 +0700

    Done with objective model

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,18 +1,16 @@
 class ObjectivesController < ApplicationController
-	before_action :set_course, :only=>[:index, :create]
+	before_action :set_course, :only=>[:index, :create, :destroy]
+	before_action :set_objective, :only=>[ :edit, :update, :destroy]
+	
 	def index
-
-		@objectives=@course.objectives
-
-
-	end
-	def new
 		@objective=@course.objectives.new
+		@objectives=@course.objectives
 	end
+	
 	def create
 		@objective=@course.objectives.new(objective_params)
 		if @objective.save
-			redirect_to @objective
+			redirect_to course_objectives_path(@course)
 		else
 			render :create
 		end
@@ -23,10 +21,17 @@ class ObjectivesController < ApplicationController
 	def update
 		
 	end
+	def destroy
 
+		@objective.destroy
+		redirect_to course_objectives_path(@course)
+	end
 	private
 	def set_course
 		@course=Course.find(params[:course_id])
+	end
+	def set_objective
+		@objective=Objective.find(params[:id])
 	end
 	def objective_params
 		params.require(:objective).permit(:description)
